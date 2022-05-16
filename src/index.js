af4b27468b1cafa1340b39baf6d3bacc2a60aed6
Franziska-F <f.fouchs@gmail.com>
2022-05-16 17:44:55 +0200

    Basic style of nav bar and header

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+import {
+  css,
+  Global,
+} from '@emotion/react';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          font-family: 'Roboto', sans-serif;
+
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+        *,
+        ::before,
+        ::after {
+          box-sizing: border-box;
+        }
+      `}
+    />
+    <App />
+  </React.StrictMode>,
+);
